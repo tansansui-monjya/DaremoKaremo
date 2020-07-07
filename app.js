36a8327ef340935618013a7cc6ab4943f419f089
Moyochi <eruto_momo@icloud.com>
2020-07-07 15:02:30 +0900

    脆弱性対応

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('port',(process.env.PORT || 5000))
+
 app.set('view engine', 'jade');
 app.set('port', (process.env.PORT || 5000));
 app.use(logger('dev'));
