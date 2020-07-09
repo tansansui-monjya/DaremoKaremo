9915053a23335b046a6a27d457a490eec036d2c5
Moyochi <eruto_momo@icloud.com>
2020-07-09 11:35:43 +0900

    再競合修正

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,8 +1,6 @@
 // Peerモデルを変更
 const Peer = window.Peer;
-
 (async function main() {
-  
   // 操作がDOMをここで取得
   // 自分の
   const localVideo = document.getElementById('js-local-stream');
@@ -12,27 +10,22 @@ const Peer = window.Peer;
   // const remoteVideos = document.getElementById('js-remote-streams');
   const roomId = document.getElementById('js-room-id');
   const roomMode = document.getElementById('js-room-mode');
-
   //threevrmのcanvas読み込み
   let canvas = null;
   while(canvas == null){
   canvas = document.getElementById("canvas2").captureStream();
   console.log("est");
   }
-
   //今回使用していないのでコメントアウトする
   // const localText = document.getElementById('js-local-text');
   // const sendTrigger = document.getElementById('js-send-trigger');
   // const messages = document.getElementById('js-messages');
-
   const meta = document.getElementById('js-meta');
   const sdkSrc = document.querySelector('script[src*=skyway]');
-
   var count = 0;  // カウント
   const remoteVideos = document.getElementById('js-remote-streams'+count);
   //共有機能の変数
   const shareTrigger = document.getElementById('js-share-trigger');
-
 // //ページ読み込み完了時に動作する内容
 // document.addEventListener("DOMContentLoaded",function(){
 //   //使用デバイスから出入力デバイスを読み取る動作
@@ -56,8 +49,6 @@ const Peer = window.Peer;
 // });
 // }
 // );
-
-
   // const localStream = await navigator.mediaDevices
   //   .getUserMedia({
   //     audio: true,
@@ -65,11 +56,8 @@ const Peer = window.Peer;
   //     // video: { facingMode: 'user' }, // 液晶側のカメラ
   //   })
   //   .catch(console.error);
-
   metainnerText = `
-    
   `.trim();
-
   // 同時接続モードがSFUなのかMESHなのかをここで設定
   const getRoomModeByHash = () => (location.hash === '#sfu' ? 'sfu' : 'mesh');
   // divタグに接続モードを挿入
@@ -79,7 +67,6 @@ const Peer = window.Peer;
     'hashchange',
     () => (roomMode.textContent = getRoomModeByHash())
   );
-
   // 自分の映像と音声をlocalStreamに代入
   const localStream = await navigator.mediaDevices
     .getUserMedia({
@@ -87,19 +74,16 @@ const Peer = window.Peer;
       video: true,
     })
     .catch(console.error);
-
   // localStreamをdiv(localVideo)に挿入
   localVideo.muted = true;
   localVideo.srcObject = localStream;
   localVideo.playsInline = true;
   await localVideo.play().catch(console.error);
-
   // Peerのインスタンス作成
   const peer = (window.peer = new Peer({
     key: window.__SKYWAY_KEY__,
     debug: 3,
   }));
-
   // 「div(joinTrigger)が押される＆既に接続が始まっていなかったら接続」するリスナーを設置
   joinTrigger.addEventListener('click', () => {
     // Note that you need to ensure the peer has connected to signaling server
@@ -107,15 +91,12 @@ const Peer = window.Peer;
     if (!peer.open) {
       return;
     }
-
     // 部屋に接続するメソッド（joinRoom）
     const room = peer.joinRoom(roomId.value, {
       mode: getRoomModeByHash(),
       // stream: localStream,
       stream: canvas,　//canvasをstreamに渡すと相手に渡せる
     });
-  
-
 /*
     // 部屋に接続できた時（open）に一度だけdiv(messages)に=== You joined ===を表示
     room.once('open', () => {
@@ -140,9 +121,7 @@ const Peer = window.Peer;
       newVideo.setAttribute('data-peer-id', stream.peerId);
       // 配列に追加する(remoteVideosという配列にnewVideoを追加)
       remoteVideos.append(newVideo);
-
 //      document.getElementById('rv'+count).innerHTML = remoteVideos;
-
 /*
        (変数名+count)←変数名＝newVideo　にしたい byキム兄
         var nv = v + count
@@ -151,17 +130,13 @@ const Peer = window.Peer;
 */
       // awaitはasync streamの実行を一時停止し、Promiseの解決または拒否を待ちます。
       await newVideo.play().catch(console.error);
-
       count+=1;
-
     });
-
     //今回変数messagesをindex.htmlで使用していないためコメントアウトする
     // room.on('data', ({ data, src }) => {
     //   // Show a message sent to the room and who sent
     //   messages.textContent += `${src}: ${data}\n`;
     // });
-
     // 誰かが退出した場合、div（remoteVideos）内にある任意のdata-peer-idがついたvideoタグの内容を空にして削除する
     room.on('peerLeave', peerId => {
       const remoteVideo = remoteVideos.querySelector(
@@ -172,22 +147,18 @@ const Peer = window.Peer;
       remoteVideo.remove();
 //      messages.textContent += `=== ${peerId} left ===\n`;
     });
-
     // for closing myself(自分の退出)
     room.once('close', () => {
-
       //今回sendTriggerおよびmessagesを使用していないためコメントアウトする
       // メッセージ送信ボタンを押せなくする
       // sendTrigger.removeEventListener('click', onClickSend);
       // messages.textContent += '== You left ===\n';
-
       Array.from(remoteVideos.children).forEach(remoteVideo => {
         remoteVideo.srcObject.getTracks().forEach(track => track.stop());
         remoteVideo.srcObject = null;
         remoteVideo.remove();
       });
     });
-
     // ボタン（sendTrigger）を押すとonClickSendを発動
     // sendTrigger.addEventListener('click', onClickSend);
     // ボタン（leaveTrigger）を押すとroom.close()を発動
@@ -197,7 +168,6 @@ const Peer = window.Peer;
       window.open('https://www.google.com/', '_self').close();
     }, 
     { once: true });
-
     //今回テキストメッセージを送信しないのでコメントアウトする
     // テキストメッセージを送る処理
     // function onClickSend() {
@@ -206,19 +176,15 @@ const Peer = window.Peer;
     //   messages.textContent += `${peer.id}: ${localText.value}\n`;
     //   localText.value = '';
     // }
-
     //追加機能share
     var copy_url = document.URL
     shareTrigger.addEventListener('click',function(){
       var shared_url = window.jsLib.shared_url_copy(copy_url);
       alert("コピーできました");
     });
-    
   });
-
   const toggleCamera = document.getElementById('js-toggle-camera');
   const toggleMicrophone = document.getElementById('js-toggle-microphone');
-
   //ボタン押した時のカメラ関係の動作
 toggleCamera.addEventListener('click', () => {
   const videoTracks = localStream.getVideoTracks()[0];
@@ -227,7 +193,6 @@ toggleCamera.addEventListener('click', () => {
   console.log()
   toggleCamera.textContent = `カメラ${videoTracks.enabled ? 'ON' : 'OFF'}`;
 });
-
 //ボタン押した時のマイク関係の動作
 toggleMicrophone.addEventListener('click', () => {
   const audioTracks = localStream.getAudioTracks()[0];
@@ -235,10 +200,6 @@ toggleMicrophone.addEventListener('click', () => {
   console.log(audioTracks.enabled)
   toggleMicrophone.textContent = `マイク${audioTracks.enabled ? 'ON' : 'OFF'}`;
 });
-
-
-
-
-
   peer.on('error', console.error);
 })();
+
