bac9bbd760280fc049bef28b3b5bb449f243c90f
nekozuki0114 <nekozuki.090@gmail.com>
2020-07-28 10:53:57 +0900

    ルーティング直した

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -13,6 +12,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'jade');
+
 app.set('port', (process.env.PORT || 5000));
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //　/にアクセスしたら↓indexRouter
 app.use('/', indexRouter);
 //usersにアクセスしたら↓usersRouter
-app.use('/users', usersRouter);
+app.use('/HP', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+// middleware that is specific to this router
+router.use(function timeLog (req, res, next) {
+  console.log('Time: ', Date.now())
+  next()
+})
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/index', function(req, res, next) {
+  res.render('index', { title: 'index' });
+});
+
+/* GET users listing. */
+router.get('/HP', function(req, res, next) {
+  res.render('HP', { title: 'HP' });
 });
 
 module.exports = router;
