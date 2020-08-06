c5f5fcb88c44886eba7718808bbef5c74c9046bd
poseidon-kimu <kaitofukin@yahoo.co.jp>
2020-08-06 12:11:09 +0900

    読み込み完了後2秒後にオーバーレイに遷移

--- public/javascripts/Loading.js
+++ public/javascripts/Loading.js
@@ -3,21 +3,20 @@ $(function() {
     
     $('#wrap').css('display','none');
     $('#loader-bg ,#loader').height(h).css('display','block');
-  });
+});
     
-  $(window).on('load',function () { //全ての読み込みが完了したら実行
-    $('#loader-bg').delay(900).fadeOut(800);
-    $('#loader').delay(600).fadeOut(300);
-    $('#wrap').css('display', 'block');
-  });
+$(window).on('load',function () { //全ての読み込みが完了したら実行
+    setTimeout('stopload()',2000)
+    // $('#loader-bg').delay(900).fadeOut(800);
+    // $('#loader').delay(600).fadeOut(300);
+});
     
-  //10秒たったら強制的にロード画面を非表示
-  $(function(){
+//10秒たったら強制的にロード画面を非表示
+$(function(){
     setTimeout('stopload()',10000);
-  });
+});
     
-  function stopload(){
-    $('#wrap').css('display','block');
+function stopload(){
     $('#loader-bg').delay(900).fadeOut(800);
     $('#loader').delay(600).fadeOut(300);
-  }
+}
