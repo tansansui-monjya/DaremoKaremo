dae0ca8eaa41f2c5c29dae4b291000b3259b0b9b
ASO1701023 <44828107+aso1701023@users.noreply.github.com>
2020-08-17 09:47:00 +0900

    roomidのハッシュ化タイミング変更

---
+++ public/javascripts/HP.js
@@ -0,0 +1,29 @@
+const room = document.getElementById("room");
+const roomId = document.getElementById("roomid");
+
+const room_make = document.addEventListener('click',() => { 
+  (async () => {
+    console.log("test");
+    let hashID = await sha256(room);
+    document.getElementById("roomid").value = hashID;
+  })();  
+	return;
+})
+
+
+// window.onbeforeunload = function(event) {
+	
+// };
+
+//ハッシュ関数
+async function sha256(str) {
+    // Convert string to ArrayBuffer
+    const buff = new Uint8Array([].map.call(str, (c) => c.charCodeAt(0))).buffer;
+    // Calculate digest
+    const digest = await crypto.subtle.digest('SHA-256', buff);
+    // Convert ArrayBuffer to hex string
+    // (from: https://stackoverflow.com/a/40031979)
+    let hash = [].map.call(new Uint8Array(digest), x => ('00' + x.toString(16)).slice(-2)).join('');
+    console.log(hash)
+    return hash
+  }
