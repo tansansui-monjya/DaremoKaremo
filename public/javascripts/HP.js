31c50ee873a6327dedd02db3014da13a0f137921
Moyochi <eruto_momo@icloud.com>
2020-08-19 15:02:59 +0900

    roomidランダム生成

--- public/javascripts/HP.js
+++ public/javascripts/HP.js
@@ -1,19 +1,33 @@
 const room = document.getElementById("room");
 const roomId = document.getElementById("roomid");
-
 const room_make = document.addEventListener('click',() => { 
-  (async () => {
-    console.log("test");
-    let hashID = await sha256(room);
-    document.getElementById("roomid").value = hashID;
-  })();  
-	return;
+  if(room.value == null){
+    (async () => {
+      roomId.value = Math.random().toString(36).slice(-8)
+      console.log("test");
+      let hashID = await sha256(roomId);
+      document.getElementById("roomid").value = hashID;
+    })();  
+    return;
+  }
+  else if(room.value == ""){
+    (async () => {
+      roomId.value = Math.random().toString(36).slice(-8)
+      console.log("test");
+      let hashID = await sha256(roomId);
+      document.getElementById("roomid").value = hashID;
+    })();  
+    return;
+  }
+  else{
+    (async () => {
+      console.log("test");
+      let hashID = await sha256(room);
+      document.getElementById("roomid").value = hashID;
+    })();  
+    return;
+  }
 })
-
-
-// window.onbeforeunload = function(event) {
-	
-// };
 
 //ハッシュ関数
 async function sha256(str) {
