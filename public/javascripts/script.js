ae36c12938cd6bc860239c4ee5ff8f11fd3cbfa9
ASO1701023 <44828107+aso1701023@users.noreply.github.com>
2020-10-13 10:21:58 +0900

    Update script.js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -183,10 +183,10 @@ toggleSpeaker.addEventListener('click', () => {
   console.log(remoteVideo_count)
   if(remoteVideo_count == 0){
     if(toggleSpeaker.className == 'speaker-btn_OFF'){
-      toggleSpeaker.setAttribute('class','speaker-btn_OFF');
+      toggleSpeaker.className = 'speaker-btn';
     }
-    else if(toggleSpeaker.className = 'speaker-btn'){
-      toggleSpeaker.setAttribute('class','speaker-btn');
+    else if(toggleSpeaker.className == 'speaker-btn'){
+      toggleSpeaker.className = 'speaker-btn';
     }
   }
   else {
