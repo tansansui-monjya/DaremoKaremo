a45daeb2f8cea06b86abbbc4c8d0e86dc50b0ab9
poseidon-kimu <kaitofukin@yahoo.co.jp>
2020-10-23 12:28:57 +0900

    通話画面の画面分割60%完了

--- public/javascripts/threevrm.js
+++ public/javascripts/threevrm.js
@@ -11,10 +11,10 @@ let renderer = new THREE.WebGLRenderer();
 var obj = document.getElementById("js-remote-streams");
 var w = obj.getBoundingClientRect().width;
 var h = obj.getBoundingClientRect().height;
-var w1 = w/2;
-var h1 = h/2;
+// var w1 = w/2.5;
+// var h1 = h/2;
 //renderer.setSize(window.innerWidth * 0.5, window.innerHeight * 0.4);
-renderer.setSize(w1, h1);
+renderer.setSize(w, h);
 let element = document.getElementById('js-remote-streams').appendChild( renderer.domElement );
 element.id = 'canvas2'
 element.class = 'canvas3'
