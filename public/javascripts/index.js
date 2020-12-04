5e05eeaa359d5cb4ea0a92fcd193ee8f3ab946da
poseidon-kimu <kaitofukin@yahoo.co.jp>
2020-12-04 15:53:26 +0900

    ログ削除等コード整理

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,8 @@
 //ハッシュ関数
 window.onload = async function(){
+  //hiddenのroomIdを取得して日時をハッシュ化したものを設定する
   const roomId = document.getElementById("roomid");
+  //日時取得
   const date1 = new Date();
   const date2 = date1.getFullYear() + "" +
     (date1.getMonth() + 1)  + "" +
@@ -9,16 +11,11 @@ window.onload = async function(){
     date1.getMinutes() + "" +
     date1.getSeconds() + "" +
     date1.getMilliseconds();
-  console.log(date2);
-
-  // // Convert string to ArrayBuffer
+  //ハッシュ化-asyncじゃなきゃだめっぽい
   const buff = new Uint8Array([].map.call(date2, (c) => c.charCodeAt(0))).buffer;
-  // Calculate digest
   const digest = await crypto.subtle.digest('SHA-256', buff);
-  // Convert ArrayBuffer to hex string
   // (from: https://stackoverflow.com/a/40031979)
   let hash = [].map.call(new Uint8Array(digest), x => ('00' + x.toString(16)).slice(-2)).join('');
-  console.log(hash);
+  //値設定
   roomId.value = hash;
-  // roomId.value = date2;
 }
